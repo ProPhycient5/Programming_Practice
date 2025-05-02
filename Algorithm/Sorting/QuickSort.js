786c49eafd82a0494ce53d5ed04c7a2840758301
Sawan Kuma Das <71059909+ProPhycient5@users.noreply.github.com>
2025-05-02 20:10:44 +0530

    added quick sort

---
+++ Algorithm/Sorting/QuickSort.js
@@ -0,0 +1,31 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(arr){
+  if (arr.length <= 1)
+  return arr;
+  
+  let pivot = arr[0];
+  let left = []; right = [];
+  
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] <= pivot){
+      left.push(arr[i])
+    }else{
+      right.push(arr[i])
+    }
+  }
+  
+  return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+
+console.log(quickSort(numbers));
+
+//Time Complexity :
+//Average case - O(n log n)
+//Worst case - O(n ^ 2)
+
+//Space Complexity - 
+//Average case - O(log n)
+//Worst case - O(n)
+
