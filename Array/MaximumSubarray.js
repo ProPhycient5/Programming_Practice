09eab9bfdebd784c9df8db43b539686f7cb7fd14
Sawan Kuma Das <71059909+ProPhycient5@users.noreply.github.com>
2025-06-07 20:08:01 +0530

    kadane's algorithm

--- Array/MaximumSubarray.js
+++ Array/MaximumSubarray.js
@@ -3,23 +3,37 @@ const nums = [-2,1,-3,4,-1,2,1,-5,4] //Input:
 // Output: 6
 // Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 
-function maxSubArray(nums){
-    let sum = 0, maxNum = nums[0];
-    let startIndex = 0, endIndex = 0;
+function maxSubArray2(nums) {
+  let maxSum = nums[0];
+  let currentSum = nums[0];
 
-    for (let i = 0; i < nums.length; i++){
-        sum = sum + nums[i];
-        if (sum > maxNum){
-            maxNum = sum;
-            lastIndex = i;     
-        }
-        if (sum < 0 ){
-            sum = 0;
-            startIndex = i + 1;
-        }
+  let startIdx = 0;
+  let tempStart = 0;
+  let endIdx = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (currentSum < 0) {
+      currentSum = nums[i];
+      tempStart = i;
+    } else {
+      currentSum += nums[i];
     }
-   return {sum, subarray: nums.slice(startIndex, lastIndex + 1)};
+
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+      startIdx = tempStart;
+      endIdx = i;
+    }
+  }
+
+  return { maxSum, subArray: nums.slice(startIdx, endIdx + 1) };
 }
+
+
+console.log(maxSubArray2([5,4,-1,7,8]))
+
+// The above code handles all test cases, if all the array contains -ve numbers.
+//[-3, -2, -1] → maxSum: -1, subarray: [-1] 
 
 //Time Complexity - O(n)
 //Space Complexity - O(1)
